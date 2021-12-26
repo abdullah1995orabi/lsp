257259947d0c09e4b1f032e31ebc5d55a5676dc9
ad-nah <ahmad.alnahlawi@icloud.com>
2021-12-26 23:36:06 +0100

    validatetor,  passwordConfirmation function added

--- client/src/Components/Auth/Registrieren.js
+++ client/src/Components/Auth/Registrieren.js
@@ -1,43 +1,80 @@
 import React, { Component } from "react";
 import axios from "axios";
-export default class Registrieren  extends Component {
+import { Navigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import {ErrorMessage} from "../Messages/Messages"
+export default class Registrieren extends Component {
   constructor(props) {
     super(props);
-   this.state={
-     nachname:"",
-     vorname:"",
-     email:"",
-     password:"",
-     accountType:"",
-     plz:"",
-     strnr:"",
-   };
-this.registerHandler=this.registerHandler.bind(this);
+    this.state = {
+      data: {
+        nachname: "",
+        vorname: "",
+        email: "",
+        password: "",
+        passwordConfirmation: "",
+        accountType: "",
+        plz: "",
+        strnr: "",
+      },
+      redirect: false,
+      errors: {},
+    };
+    this.registerHandler = this.registerHandler.bind(this);
   }
 
-  
+  validate(data) {
+    let error = {};
+    if (!data.vorname) error.vorname = "fistName is required!";
+    if (!data.nachname) error.nachname = "lastName is required!";
+    if (!data.accountType) error.accountType = "accountType is required!";
+    if (!data.plz) error.plz = "plz is required!";
+    if (!data.strnr) error.strnr = "strnr is required!";
+
+    if (!data.email) error.email = "email is required!";
+    if (!data.password) error.password = "password is required!";
+    if (this.state.data.password !== this.state.data.passwordConfirmation)
+      error.passwordConfirmation = "password did not matched!";
+
+    return error;
+  }
+
   registerHandler(e) {
-    axios
-    .post("/api/register", this.state)
-    .then((x) => x.data)
-    .then((data) => {
-     alert(data.message)
-    }).catch(err => {
-      alert(err.response.data.message)
-    });
-}
+    let errors = this.validate(this.state.data);
+    this.setState({ errors: errors });
+
+    if (Object.keys(errors).length === 0) {
+      axios
+        .post("/api/register", this.state.data)
+        .then((x) => x.data)
+        .then((data) => {
+          this.setState({ redirect: true });
+          toast(data.message);
+        })
+        .catch((err) => {
+          toast.error(err.response.data.message);
+        });
+    }
+  }
 
   render() {
     return (
       <div className="container">
+        {this.state.redirect && <Navigate to="/" />}
         <h1>Register Seite</h1>
         <p>registriere dich bei uns!</p>
         <hr />
         <label htmlFor="nachname">
+          {this.state.errors.nachname && <ErrorMessage message={this.state.errors.nachname}/>}
+        
           <b>Nachname</b>
         </label>
         <input
-          onChange={(e)=> this.setState({nachname: e.target.value})}
+          onChange={(e) =>
+            this.setState({
+              data: { ...this.state.data, nachname: e.target.value },
+            })
+          }
           type="text"
           placeholder="Enter Nachname"
           name="nachname"
@@ -45,10 +82,16 @@ this.registerHandler=this.registerHandler.bind(this);
           required
         />
         <label htmlFor="vorname">
+          {this.state.errors.vorname && this.state.errors.vorname}
+
           <b>Vorname</b>
         </label>
         <input
-          onChange={(e)=> this.setState({vorname: e.target.value})}
+          onChange={(e) =>
+            this.setState({
+              data: { ...this.state.data, vorname: e.target.value },
+            })
+          }
           type="text"
           placeholder="Enter Vorname"
           name="vorname"
@@ -56,10 +99,14 @@ this.registerHandler=this.registerHandler.bind(this);
           required
         />
         <label htmlFor="plz/ort">
+          {this.state.errors.plz && this.state.errors.plz}
+
           <b>plz/Ort</b>
         </label>
         <input
-          onChange={(e)=> this.setState({plz: e.target.value})}
+          onChange={(e) =>
+            this.setState({ data: { ...this.state.data, plz: e.target.value } })
+          }
           type="text"
           placeholder="Plz/Ort"
           name="plz"
@@ -68,10 +115,16 @@ this.registerHandler=this.registerHandler.bind(this);
         />
 
         <label htmlFor="strassen_Nr">
+          {this.state.errors.strnr && this.state.errors.strnr}
+
           <b>Straßen Nr</b>
         </label>
         <input
-          onChange={(e)=> this.setState({strnr: e.target.value})}
+          onChange={(e) =>
+            this.setState({
+              data: { ...this.state.data, strnr: e.target.value },
+            })
+          }
           type="text"
           placeholder="Nibelungenstraße 6"
           name="strnr"
@@ -79,11 +132,17 @@ this.registerHandler=this.registerHandler.bind(this);
           required
         />
         <label htmlFor="email">
+          {this.state.errors.email && this.state.errors.email}
+
           <b>Email</b>
         </label>
         <input
-          onChange={(e) => this.setState({ email: e.target.value })}
-          type="text"
+          onChange={(e) =>
+            this.setState({
+              data: { ...this.state.data, email: e.target.value },
+            })
+          }
+          type="email"
           placeholder="Enter Email"
           name="email"
           id="email"
@@ -91,19 +150,54 @@ this.registerHandler=this.registerHandler.bind(this);
         />
 
         <label htmlFor="password">
+          {this.state.errors.password && this.state.errors.password}
+
           <b>Password</b>
         </label>
         <input
-          onChange={(e) => this.setState({ password: e.target.value })}
+          onChange={(e) =>
+            this.setState({
+              data: { ...this.state.data, password: e.target.value },
+            })
+          }
           type="password"
           placeholder="Enter Password"
           name="password"
           id="password"
           required
         />
+
+        <label htmlFor="password">
+          {this.state.errors.passwordConfirmation &&
+            this.state.errors.passwordConfirmation}
+
+          <b>Password Wiederholung</b>
+        </label>
+        <input
+          onChange={(e) =>
+            this.setState({
+              data: {
+                ...this.state.data,
+                passwordConfirmation: e.target.value,
+              },
+            })
+          }
+          type="password"
+          placeholder="Password Wiederholung"
+          name="passwordConfirmation"
+          id="passwordConfirmation"
+          required
+        />
+
+        {this.state.errors.accountType && this.state.errors.accountType}
+
         <select
           // onChange={function(e){console.log(e.target.value)}}
-          onChange={(e) => this.setState({ accountType: e.target.value })}
+          onChange={(e) =>
+            this.setState({
+              data: { ...this.state.data, accountType: e.target.value },
+            })
+          }
           className=" auswahl form-select-lg mb-3 col-12 "
           aria-label=".form-select-lg example"
         >
