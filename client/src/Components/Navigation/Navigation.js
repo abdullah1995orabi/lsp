c85497bf07761703f8b69e5c730e6b602f0f85dd
ad-nah <ahmad.alnahlawi@icloud.com>
2021-12-26 23:39:44 +0100

    change: code style

--- client/src/Components/Navigation/Navigation.js
+++ client/src/Components/Navigation/Navigation.js
@@ -17,7 +17,8 @@ export default class Navigation extends Component {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon"></span>
+          MENU
+          {/* <span className="navbar-toggler-icon "></span> */}
         </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
